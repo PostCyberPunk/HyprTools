150400ab88af7c17f9e82ba5b02f3d179b0ab9b9
PostCyberPunk <lyhguest2@outlook.com>
2024-01-08 09:30:26 +0800

    feat(term):toogle term

--- Lib/cmds.sh
+++ Lib/cmds.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+# hpdp= hyprctl dispatch
+get_active_workspace() {
+	return $(hyprctl -j clients | jq -c '.id')
+}
+hl_notify() {
+	hyprctl notify -1 1000 "rgb(ff1ea3)" "$1"
+}
 clipboard_manager() {
 	result=$(cliphist list | fzf --reverse -m)
 	echo "$result" | while read -r line; do
 		printf "$line" | cliphist delete
 	done
 }
-$1
+toggle_term() {
+	result=$(hyprctl -j clients | jq -c '.[] | select(.initialTitle == "FTQCT") | .pid')
+	if [[ -z $result ]]; then
+		kitty -T "FTQCT" --class floating &
+		exit 0
+	else
+		hyprctl dispatch togglespecialworkspace FTQCT
+	fi
+}
+if [[ ! -z "$1" ]]; then
+	$1 "${@:2}"
+fi
