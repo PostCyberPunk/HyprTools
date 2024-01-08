127f25bf9da28e7e3349a69491410d9d09fb5902
PostCyberPunk <lyhguest2@outlook.com>
2024-01-08 12:23:32 +0800

    fix(wallpaper)

--- WallpaerSwitcher.sh
+++ WallpaerSwitcher.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-source ~/dotfiles/mysh/change_wallpaper.sh
-cd $1
+wallpaper_dir=$HOME/Pictures/wallpapers
+source ~/.config/hypr/scripts/ChangeWallpaper.sh
+cd $wallpaper_dir
 result=$(find . -type f|fzf \
 --reverse --header="WallPaperSwitcher" --preview-window=44% \
 --preview='kitten icat --clear --transfer-mode=memory --place="$FZF_PREVIEW_COLUMNS"x"$FZF_PREVIEW_LINES"@$(math $COLUMNS+5)x1 --align center --stdin=no -z -1 {}')
